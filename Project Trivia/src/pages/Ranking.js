487b2d66d1787e96373607c0bcbfc017a1545627
Micaeli Teixeira <luime6@gmail.com>
2021-02-04 20:36:51 -0300

    style: ranking pages

--- Project Trivia/src/pages/Ranking.js
+++ Project Trivia/src/pages/Ranking.js
@@ -2,29 +2,36 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import './Ranking.css';
+import "../style/ranking.css"
+
 
 class Ranking extends Component {
   render() {
     const { ranking } = this.props;
     return (
-      <div className="container-ranking">
-        <h1 data-testid="ranking-title">Ranking</h1>
-        <div className="list">
-          {ranking
-            .sort((a, b) => b.score - a.score)
-            .map((player, index) => (
-              <div key={ index }>
-                <img src={ player.picture } alt={ player.name } />
-                <p data-testid={ `player-name-${index}` }>{player.name}</p>
-                <p data-testid={ `player-score-${index}` }>{player.score}</p>
-              </div>
-            ))}
-        </div>
-        <div className="button-ranking">
-          <Link to="/">
-            <button type="button" data-testid="btn-go-home">JOGAR NOVAMENTE</button>
-          </Link>
+      <div>
+        <p className= "title"> Trivia</p>
+        <p className= "subtitle"> The Game</p>
+        <div className="container">
+          <div className="container-ranking">
+            <h1 data-testid="ranking-title" className="ranking-title">Ranking</h1>
+            <div className="list">
+              {ranking
+                .sort((a, b) => b.score - a.score)
+                .map((player, index) => (
+                  <div className="index-game" key={ index }>
+                    <img className="img" src={ player.picture } alt={ player.name } />
+                    <p data-testid={ `player-name-${index}` }>{player.name}</p>
+                    <p data-testid={ `player-score-${index}` }>{player.score}</p>
+                  </div>
+                ))}
+            </div>
+            <div className="button-ranking">
+              <Link to="/">
+                <button className="button rank"type="button" data-testid="btn-go-home">PLAY AGAIN</button>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     );
