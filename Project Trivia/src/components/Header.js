5029f3634125922e16038925d561f3851532d6f3
Micaeli Teixeira <luime6@gmail.com>
2021-02-04 17:47:14 -0300

    style: refactoring code and style

--- Project Trivia/src/components/Header.js
+++ Project Trivia/src/components/Header.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import './Header.css';
+import '../style/header.css'
+
 
 class Header extends Component {
   render() {
     const { name, score, hash } = this.props;
     return (
       <div className="class-header">
-        <img
-          data-testid="header-profile-picture"
-          src={ `https://www.gravatar.com/avatar/${hash}` }
-          alt="Header profile"
-          className="img-gravatar"
-        />
-        <p data-testid="header-player-name" className="text">
-          {name}
-        </p>
-        <span data-testid="header-score" className="text">
-          {score}
-        </span>
+        <div className="hello">
+          <img
+            data-testid="header-profile-picture"
+            src={ `https://www.gravatar.com/avatar/${hash}` }
+            alt="Header profile"
+            className="img-gravatar"
+          />
+          <p data-testid="header-player-name" className= "header-player-name">
+              Hello {name}!!
+          </p>
+        </div>
+        <div className="div-score">
+          <span data-testid="header-score" className="header-score" >
+           {score}
+          </span>
+        </div>
       </div>
     );
   }
--- Project Trivia/src/index.js
+++ Project Trivia/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import * as serviceWorker from './serviceWorker';
 import App from './App';
 
 ReactDOM.render(
@@ -19,4 +18,3 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
